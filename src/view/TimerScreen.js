41a0886d83f60a8e62174e46d3b0edf48f92194b
Jack Gardner <gardnerjack11@gmail.com>
2018-08-05 08:52:16 +1000

    Added cancel session button

--- src/view/TimerScreen.js
+++ src/view/TimerScreen.js
@@ -9,7 +9,7 @@ import Button from '../components/Button';
 const Container = styled.View`
   flex: 1;
   background-color: ${styles.backgroundColor};
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
 `;
 
@@ -19,6 +19,13 @@ const TimeText = styled.Text`
   color: ${timerStyle.textColor};
   align-self: center;
   margin-bottom: 32;
+`;
+
+const ButtonContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
 `;
 
 type Props = {
@@ -61,15 +68,32 @@ export default class TimerScreen extends React.Component<Props> {
     });
   }
 
+  cancelSession = () => {
+    const currentMeditationID = this.props.navigation.getParam('meditationID', null);
+    AsyncStorage.getItem('meditation', (err, result) => {
+      if (err) throw err;
+      const table = JSON.parse(result);
+      const newTable = table.filter(meditation => meditation.ID != currentMeditationID);
+      AsyncStorage.setItem('meditation', JSON.stringify(newTable));
+    })
+    this.props.navigation.navigate('Home');
+  }
+
   render() {
     return (
       <Container>
         <StatusBar barStyle="light-content"/>
         <TimeText>{moment(this.state.time).format('mm:ss')}</TimeText>
-        <Button
-          onPress={this.onPressButton}
-          title="Done"
-        />
+        <ButtonContainer>
+          <Button
+            onPress={this.cancelSession}
+            title="Cancel"
+          />
+          <Button
+            onPress={this.onPressButton}
+            title="Done"
+          />
+        </ButtonContainer>
       </Container>
     );
   }
